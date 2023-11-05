d9fb7c94e58c1cb97af6a26b1b0fac462c2af694
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-05 19:30:52 +0100

    feat Password generator

--- myscript.js
+++ myscript.js
@@ -1,1 +1,18 @@
 'use script'
+
+
+const elementName = document.getElementById('name');
+const elementSurname = document.getElementById('surname');
+const elementFavoritEcolor = document.getElementById('favorite-color');
+const elementNumber = document.getElementById ('number');
+
+elementNumber.innerHTML ="21"
+
+elementName.innerHTML = ' Il tuo nome ';
+elementSurname.innerHTML = 'Il tuo cognome ';
+elementFavoritEcolor.innerHTML = 'Il tuo colore preferito  ';
+
+
+elementName.innerHTML = prompt ('Inserisci il tuo nome')
+elementSurname.innerHTML = prompt ('Inserisci il tuo cognome')
+elementFavoritEcolor.innerHTML = prompt ('Inserisci il tuo colore preferito')
